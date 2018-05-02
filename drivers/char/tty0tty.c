3ed3791bd75896903706d645064c0f44a185d90a
anilkumar appana <anilkumar@matellio.com>
2018-05-02 18:25:11 +0530

    added mutiple virtual console support

--- drivers/char/tty0tty.c
+++ drivers/char/tty0tty.c
@@ -19,107 +19,108 @@
 #include <linux/tty.h>
 #include <linux/module.h>
 #include <linux/tty_flip.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
+#include <linux/slab.h>
 
+/*
+This is used to handle current port to send and receive data
+*/
 struct ttyprintk_port {
-	struct tty_port port;
-	struct mutex port_write_mutex;
-	int open_count;
+	struct tty_port *port;   /* port */
+	struct mutex port_write_mutex;  
 };
 
-static struct ttyprintk_port tpk_port;
+static struct ttyprintk_port tpk_port;  
 
 
-#define DRAM_SHARED_MEM         0x3f200000
-#define DRAM_SHARED_MEM_SIZE    0x100000 //place the size here
+#define DRIVER_DESC "Jailhouse virtual console driver"
+#define DRIVER_VERSION "v1.0"
 
-#define DRAM_SHARED_MEM_SIZE_TX  0xFFE00 // place the size here
-#define DRAM_SHARED_MEM_SIZE_RX  0x100   //(DRAM_SHARED_MEM+DRAM_SHARED_MEM_SIZE_TX)
+#define DRIVER_NAME "ttyprint"
+#define DEVICE_NAME  "hyp"
+#define MAJOR_NUM    0
+#define MINOR_NUM    0
+#define NUM_OF_DRIV  8   /*This is to handle number of virual consoles */
+
+#define DRAM_SHARED_MEM         0x3f200000  /* start address of shared memory region */
+#define DRAM_SHARED_MEM_SIZE    0x1000 // Maximum memory of shared memory region (4k)
+
+#define DRAM_SHARED_MEM_SIZE_TX  0xF00 //TX buffer size
+#define DRAM_SHARED_MEM_SIZE_RX  0xF8  //RX_buffer_sixe
 #define DRAM_SHARED_MEM_OFFSET_RX  DRAM_SHARED_MEM_SIZE_TX
 
 
-#define SYNC_POINTER_OFFSET (DRAM_SHARED_MEM_SIZE_TX+DRAM_SHARED_MEM_SIZE_RX) //0x6000
+#define SYNC_POINTER_OFFSET (DRAM_SHARED_MEM_SIZE_TX+DRAM_SHARED_MEM_SIZE_RX) 
 #define WRITE_PTR_OFFSET SYNC_POINTER_OFFSET 
 #define READ_PTR_OFFSET (SYNC_POINTER_OFFSET+4)
 
-static volatile unsigned char *shared_mem;
-static volatile unsigned int *shared_mem_tx_ptr =0 ;
-static volatile unsigned int *shared_mem_rx_ptr =0 ;
-static unsigned int shared_mem_init_done = 0;
-static unsigned int local_write_ptr = 0;
-static unsigned int local_read_ptr = 0;
-static volatile unsigned char *shared_mem_tx;
-static volatile unsigned char *shared_mem_rx;
+
+static volatile unsigned int *shared_mem_tx_ptr[NUM_OF_DRIV] ={0, };
+static volatile unsigned int *shared_mem_rx_ptr[NUM_OF_DRIV] ={0, };
+static unsigned int shared_mem_init_done[NUM_OF_DRIV] = {0, };
+static unsigned int local_write_ptr[NUM_OF_DRIV] = {0, };
+static unsigned int local_read_ptr[NUM_OF_DRIV] = {0, };
+static volatile unsigned char *shared_mem_tx[NUM_OF_DRIV];
+static volatile unsigned char *shared_mem_rx[NUM_OF_DRIV];
+static void *shared_virt[NUM_OF_DRIV];
+static int device_index[NUM_OF_DRIV];
+
+void periodic_poll_tx(unsigned long data);
+int g_time_interval = 1000;
+struct timer_list g_timer_shared_mem;
+
+DEFINE_TIMER(g_timer_shared_mem, periodic_poll_tx,0,0);
 
 
-/*
- * Our simple preformatting supports transparent output of (time-stamped)
- * printk messages (also suitable for logging service):
- * - any cr is replaced by nl
- * - adds a ttyprintk source tag in front of each line
- * - too long message is fragmented, with '\'nl between fragments
- * - TPK_STR_SIZE isn't really the write_room limiting factor, because
- *   it is emptied on the fly during preformatting.
- */
+
+static struct tty_port **shared_mem_port = 0;
+
 #define TPK_STR_SIZE 508 /* should be bigger then max expected line length */
 #define TPK_MAX_ROOM 4096 /* we could assume 4K for instance */
-static int tpk_curr;
 
-static char tpk_buffer[TPK_STR_SIZE + 4];
-
-static void tpk_flush(void)
-{
-	if (tpk_curr > 0) {
-		tpk_buffer[tpk_curr] = '\0';
-		pr_info("[U] %s\n", tpk_buffer);
-		tpk_curr = 0;
-	}
-}
-
-static int tpk_printk(const unsigned char *buf, int count)
-{
-	int i = tpk_curr;
-
-	if (buf == NULL) {
-		tpk_flush();
-		return i;
-	}
-
-	for (i = 0; i < count; i++) {
-		if (tpk_curr >= TPK_STR_SIZE) {
-			/* end of tmp buffer reached: cut the message in two */
-			tpk_buffer[tpk_curr++] = '\\';
-			tpk_flush();
-		}
-
-		switch (buf[i]) {
-		case '\r':
-			tpk_flush();
-			if ((i + 1) < count && buf[i + 1] == '\n')
-				i++;
-			break;
-		case '\n':
-			tpk_flush();
-			break;
-		default:
-			tpk_buffer[tpk_curr++] = buf[i];
-			break;
-		}
-	}
-
-	return count;
-}
 
 /*
  * TTY operations open function.
+ * This function will be invoked with opening device node from minicom.
+
+
  */
 static int tpk_open(struct tty_struct *tty, struct file *filp)
 {
+	int index = tty->index;
+	int i;
+	printk("\ntpk open..\n");
 	tty->driver_data = &tpk_port;
 	
-	if(tpk_port.open_count == 0)
-		tpk_port.open_count++;
+	for(i=0; i<NUM_OF_DRIV; i++)
+	{
+		if(i == index)
+		  device_index[i] = 1; 
+	}
+        /*This condition is to initialize shared memory region when we open port for the first time */
+	if(shared_mem_init_done[index] == 0)
+        {
+		
+           shared_virt[index] = ioremap((DRAM_SHARED_MEM + 
+			(index*DRAM_SHARED_MEM_SIZE)), DRAM_SHARED_MEM_SIZE);
 
-	return tty_port_open(&tpk_port.port, tty, filp);
+           //shared_mem[index] = (volatile unsigned char *)shared_virt;
+
+           shared_mem_tx[index] = (volatile unsigned char *)shared_virt[index];
+           
+	   shared_mem_rx[index] = (volatile unsigned char *)(shared_virt[index]+DRAM_SHARED_MEM_OFFSET_RX);
+           shared_mem_tx_ptr[index] = (unsigned int *)(shared_virt[index]+WRITE_PTR_OFFSET);
+
+           shared_mem_rx_ptr[index] = (unsigned int *)(shared_virt[index]+ 
+							READ_PTR_OFFSET);                  
+	   shared_mem_init_done[index] = 1;
+        }
+
+	mod_timer(&g_timer_shared_mem, jiffies + msecs_to_jiffies(g_time_interval));
+	
+
+	return tty_port_open(&tpk_port.port[index], tty, filp);
 }
 
 /*
@@ -128,24 +129,25 @@ static int tpk_open(struct tty_struct *tty, struct file *filp)
 static void tpk_close(struct tty_struct *tty, struct file *filp)
 {
 	struct ttyprintk_port *tpkp = tty->driver_data;
-
+	int index = tty->index;
+	del_timer(&g_timer_shared_mem);
+	
 	mutex_lock(&tpkp->port_write_mutex);
-	/* flush tpk_printk buffer */
-	tpk_printk(NULL, 0);
+	
+	shared_mem_init_done[index] = 0;
 	mutex_unlock(&tpkp->port_write_mutex);
 
-	tty_port_close(&tpkp->port, tty, filp);
-
-	if(tpkp->open_count != 0)
-		tpkp->open_count =0;
+	tty_port_close(&tpkp->port[index], tty, filp);
 		
 }
+
+
 
 /*
  * TTY operations write function.
  */
 /* This function will get invoked when user gives the input to 
-   the kernel on the curretn terminal
+   the kernel on the current terminal
    
    @tty   - Current terminal port 
    @buf   - Character to push
@@ -157,84 +159,75 @@ static int tpk_write(struct tty_struct *tty,
 		const unsigned char *buf, int count)
 {
 	struct ttyprintk_port *tpkp = tty->driver_data;
-	int ret = -EINVAL;
 	int i;
+	int index = tty->index;
+	
 	
 	if(!tpkp)
 		return -ENODEV;	
 	
-	if(!tpkp->open_count)
-		goto exit;
-
 	/* exclusive use of tpk_printk within this tty */
 	mutex_lock(&tpkp->port_write_mutex);
 
-	/*For the first time accessing the Shared memory region*/
-	if(shared_mem_init_done == 0)
-        {
-		
-           void *shared_virt = ioremap(DRAM_SHARED_MEM, DRAM_SHARED_MEM_SIZE);
-           shared_mem = (volatile unsigned char *)shared_virt;
-	
-	   shared_mem_tx = (char *)shared_virt;
-           shared_mem_rx = (char *)(shared_virt+ DRAM_SHARED_MEM_OFFSET_RX);
-	
-           shared_mem_tx_ptr = (unsigned int *)(shared_virt + WRITE_PTR_OFFSET);
-
-	   shared_mem_rx_ptr = (unsigned int *)(shared_virt+ READ_PTR_OFFSET);		   shared_mem_init_done = 1;	
-	}
-
-// Non-Root Cell to Root  Cell print
-	{
-
-	int num_char_to_read = 0;
-        int shared_mem_current_loc =*(shared_mem_tx_ptr);
-
-	 if (local_read_ptr > shared_mem_current_loc)
-        {
-
-                num_char_to_read = (DRAM_SHARED_MEM_SIZE_TX - local_read_ptr)+ shared_mem_current_loc;
-        }
-        else
-        	num_char_to_read = shared_mem_current_loc - local_read_ptr;
-
-	  for(i=0; i<num_char_to_read; i++)
-	  {
-
-	    tty_insert_flip_string(&tpkp->port,(char*)(shared_mem_tx + 
-						      local_read_ptr), 1);	
-	    tty_flip_buffer_push(&tpkp->port);
-	    local_read_ptr += 1;
-	  
-  	   if(local_read_ptr == DRAM_SHARED_MEM_SIZE_TX)
-                        local_read_ptr=0;
-	  }
-	}
-	
 	/// Root ==> Non-Root Cell
 
 	for (i=0;i<count;i++)
 	{
 
-	*(shared_mem_rx + local_write_ptr) = *(buf+i);	
-	tty_insert_flip_string(&tpkp->port, (buf+i), 1);
-        tty_flip_buffer_push(&tpkp->port);
-	local_write_ptr += 1;
-	if(local_write_ptr == DRAM_SHARED_MEM_SIZE_RX)
-               local_write_ptr = 0;
-
-	*(shared_mem_rx_ptr) = local_write_ptr;
+	    *(shared_mem_rx[index] + local_write_ptr[index]) = *(buf+i);	
+	    tty_insert_flip_string(&tpkp->port[index], (buf+i), 1);
+            tty_flip_buffer_push(&tpkp->port[index]);
+	    local_write_ptr[index] += 1;
+            /*check if buffer is reached maximum*/    
+	    if(local_write_ptr[index] == DRAM_SHARED_MEM_SIZE_RX)
+                  local_write_ptr[index] = 0;
+              /* This is store number of characters written into shared memory from root cell to non rootcell*/ 
+	    *(shared_mem_rx_ptr[index]) = local_write_ptr[index];
 	}
-	
 	mutex_unlock(&tpkp->port_write_mutex);
 
 	return count;
-
-exit:
-	mutex_unlock(&tpkp->port_write_mutex);
-	return ret;
 }
 
+/*
+*  This is to read shared memory region from non rootcell to rootcell if there is any data available in shared memory
+* region. Its call back function which we will be invoked every 10 seconds to read data.
+*/
+void periodic_poll_tx(unsigned long data)
+{
+	int i,index;	
+	int num_char_to_read = 0;
+        unsigned int shared_mem_current_loc;
+
+	for(index=0; index<NUM_OF_DRIV; index++)
+	{
+	    if(device_index[index] == 1)
+	        shared_mem_current_loc = *(shared_mem_tx_ptr[index]);
+	    else
+	        continue;
+/* Non Root cell ===> Root cell  */
+
+            if (local_read_ptr[index] > shared_mem_current_loc)
+            {
+                num_char_to_read = (DRAM_SHARED_MEM_SIZE_TX - local_read_ptr[index])+ shared_mem_current_loc;
+            }
+            else
+            {
+                num_char_to_read = shared_mem_current_loc-local_read_ptr[index];
+            } 
+	    for(i=0; i<num_char_to_read; i++)
+            {
+            	  tty_insert_flip_string(shared_mem_port[index],
+		    (char*)((shared_mem_tx[index]) + local_read_ptr[index]), 1);
+               	  
+		  tty_flip_buffer_push(shared_mem_port[index]);
+            		local_read_ptr[index] += 1;
+	    	    if(local_read_ptr[index] == DRAM_SHARED_MEM_SIZE_TX)
+               	        	 local_read_ptr[index]=0;
+            }
+	}
+	mod_timer(&g_timer_shared_mem, jiffies + msecs_to_jiffies(g_time_interval));
+}
 /*
  * TTY operations write_room function.
  */
@@ -253,7 +246,6 @@ static int tpk_ioctl(struct tty_struct *tty,
 
 	if (!tpkp)
 		return -EINVAL;
-
 	switch (cmd) {
 	/* Stop TIOCCONS */
 	case TIOCCONS:
@@ -279,51 +271,79 @@ static struct tty_driver *ttyprintk_driver;
 static int __init ttyprintk_init(void)
 {
 	int ret = -ENOMEM;
+	int i;
 
 	mutex_init(&tpk_port.port_write_mutex);
 
-	ttyprintk_driver = tty_alloc_driver(1,
+	/*ttyprintk_driver = tty_alloc_driver(NUM_OF_DRIV,
 			TTY_DRIVER_RESET_TERMIOS |
-			TTY_DRIVER_REAL_RAW |
-			TTY_DRIVER_UNNUMBERED_NODE);
-	if (IS_ERR(ttyprintk_driver))
-		return PTR_ERR(ttyprintk_driver);
+			TTY_DRIVER_REAL_RAW );*/
 
-	tty_port_init(&tpk_port.port);
-	tpk_port.port.ops = &null_ops;
+	ttyprintk_driver = alloc_tty_driver(NUM_OF_DRIV);	
+	
+	if (!ttyprintk_driver)
+		return -ENOMEM;
 
-	ttyprintk_driver->driver_name = "ttyprint";
-	ttyprintk_driver->name = "hyp";
-	ttyprintk_driver->major = 240;//TTYAUX_MAJOR;
-	ttyprintk_driver->minor_start = 3;
+	tpk_port.port = kmalloc(NUM_OF_DRIV*sizeof(struct tty_port), GFP_KERNEL);
+
+	shared_mem_port = (struct tty_port **)kmalloc(NUM_OF_DRIV*
+				         sizeof(struct tty_port*), GFP_KERNEL);
+
+	for(i=0; i<NUM_OF_DRIV; i++)
+	{
+		shared_mem_port[i] = kmalloc(sizeof(struct tty_port),
+						GFP_KERNEL);
+	}
+	ttyprintk_driver->driver_name = DRIVER_NAME;
+	ttyprintk_driver->name = DEVICE_NAME;
+	ttyprintk_driver->major = MAJOR_NUM;
+	ttyprintk_driver->minor_start = MINOR_NUM;
 	ttyprintk_driver->type = TTY_DRIVER_TYPE_CONSOLE;
+	ttyprintk_driver->flags = TTY_DRIVER_RESET_TERMIOS | TTY_DRIVER_REAL_RAW ;
 	ttyprintk_driver->init_termios = tty_std_termios;
 	ttyprintk_driver->init_termios.c_oflag = OPOST | OCRNL | ONOCR | ONLRET;
 	tty_set_operations(ttyprintk_driver, &ttyprintk_ops);
-	tty_port_link_device(&tpk_port.port, ttyprintk_driver, 0);
+	
+	for(i=0 ;i<NUM_OF_DRIV; i++)
+	{
+		tty_port_init(&tpk_port.port[i]);
+		shared_mem_port[i] = &tpk_port.port[i];
 
+		tpk_port.port[i].ops = &null_ops;
+		tty_port_link_device(&tpk_port.port[i], ttyprintk_driver, i);
+	
+	}
 	ret = tty_register_driver(ttyprintk_driver);
 	if (ret < 0) {
 		printk(KERN_ERR "Couldn't register ttyprintk driver\n");
 		goto error;
 	}
 
+	printk(KERN_INFO DRIVER_DESC " " DRIVER_VERSION "\n");
 	return 0;
 
 error:
 	put_tty_driver(ttyprintk_driver);
-	tty_port_destroy(&tpk_port.port);
+	for(i=0; i<NUM_OF_DRIV; i++)
+	tty_port_destroy(&tpk_port.port[i]);
 	return ret;
 }
 
 static void __exit ttyprintk_exit(void)
 {
-	tty_unregister_driver(ttyprintk_driver);
+	int i;
+
+	for(i=0; i<NUM_OF_DRIV; i++)
+	{
+		tty_port_destroy(&tpk_port.port[i]);
+	}
+	kfree(tpk_port.port);
 	put_tty_driver(ttyprintk_driver);
-	tty_port_destroy(&tpk_port.port);
+	tty_unregister_driver(ttyprintk_driver);
 }
 
 device_initcall(ttyprintk_init);
 module_exit(ttyprintk_exit);
 
+MODULE_DESCRIPTION( DRIVER_DESC );
 MODULE_LICENSE("GPL");
